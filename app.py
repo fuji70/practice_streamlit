0288970cfd5fa211d0b8800e5764aab08e4a9ae2
N.Fujiwara <Naoki.Fujiwara@sony.com>
2022-07-24 13:37:17 +0900

    [Add] 条件でUI変更

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-st.checkbox('チェックボックス')
-st.button('ボタン')
-st.selectbox('selectbox', ('option1', 'option2', 'option3'))
-st.multiselect('multiselect', ('option1', 'option2', 'option3'))
-st.radio('radio', ('option1', 'option2', 'option3'))
-st.text_input('text_input')
-st.text_area('text_area')
+check = st.checkbox('チェックボックス')
+
+if check:
+    st.button('ボタン')
+    st.selectbox('selectbox', ('option1', 'option2', 'option3'))
+    st.multiselect('multiselect', ('option1', 'option2', 'option3'))
+    st.radio('radio', ('option1', 'option2', 'option3'))
+    st.text_input('text_input')
+    st.text_area('text_area')
