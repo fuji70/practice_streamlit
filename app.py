0a847dce5238f2eb4e405888529a180911b75e2f
N.Fujiwara <Naoki.Fujiwara@sony.com>
2022-07-24 13:38:10 +0900

    [Add] サイドバー

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 check = st.checkbox('チェックボックス')
+st.sidebar.text_input('text_input')
+st.sidebar.text_area('text_area')
 
 if check:
     st.button('ボタン')
     st.selectbox('selectbox', ('option1', 'option2', 'option3'))
     st.multiselect('multiselect', ('option1', 'option2', 'option3'))
     st.radio('radio', ('option1', 'option2', 'option3'))
-    st.text_input('text_input')
-    st.text_area('text_area')
