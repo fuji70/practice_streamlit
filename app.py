14b5241211434c4b27120dcc6a56c3769f9b67e4
N.Fujiwara <Naoki.Fujiwara@sony.com>
2022-07-24 13:35:59 +0900

    [Add] widget

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("title")
-st.write("write")
-st.markdown("# Head1")
-st.markdown("## Head2")
+st.checkbox('チェックボックス')
+st.button('ボタン')
+st.selectbox('selectbox', ('option1', 'option2', 'option3'))
+st.multiselect('multiselect', ('option1', 'option2', 'option3'))
+st.radio('radio', ('option1', 'option2', 'option3'))
+st.text_input('text_input')
+st.text_area('text_area')
